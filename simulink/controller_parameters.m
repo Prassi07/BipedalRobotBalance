58bef77fd4ebaff0ec586b87bcb902b8362bf93b
Prasanna Venkatesan <25366464+Prassi07@users.noreply.github.com>
2022-12-06 15:32:28 -0500

    Working for 125N

--- simulink/controller_parameters.m
+++ simulink/controller_parameters.m
@@ -1,39 +1,36 @@
 % *************************** %
-% 4. Ground Interaction Model %
+% PD Controller Parameters    %
 % *************************** %
 
+Kp = 100;
+Kd = 10;
+
+
+% *************************** %
+% Ground Interaction Model    %
+% *************************** %
 % body mass
 m = 42; %[kg] 
 
 % gravitational acceleration
 g = 9.81; %[m/s^2]
 
-
+% Vertical component
 % ----------------------
-% 4.1 Vertical component
-% ----------------------
-
 % stiffness of ground interaction along contact normal axis
 k_gn = m*g/0.01; %[N/m]
-
 % max relaxation speed of normal ground interaction
 v_gn_max = 0.03; %[m/s]
 
+% Horizontal component
 % ------------------------
-% 4.2 Horizontal component
-% ------------------------
-
 % sliding friction coefficient
 mu_slide = 0.8; %[]
-
 % stiction to sliding transition coefficient
 mu_stick = 0.9; %[]
-
 % sliding to stiction transition velocity limit
 vLimit = 0.01; %[m/s]
-
 % stiffness of ground stiction along contact tangential
 k_gt = m*g/0.01; %[N/m]
-
 % max relaxation speed of ground stiction
 v_gt_max = 0.03; %[m/s] 
