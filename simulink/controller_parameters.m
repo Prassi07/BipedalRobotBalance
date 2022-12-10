1079af0444beeeff0e3c4bbaf35c2bede407f5b9
nemo11 <nishantmohanty18@gmail.com>
2022-12-10 10:29:51 -0500

    updated with main files

---
+++ simulink/controller_parameters.m
@@ -0,0 +1,51 @@
+% *************************** %
+% PD Controller Parameters    %
+% *************************** %
+
+Kp = 100;
+Kd = 10;
+
+Kp_pitch = 100;
+Kd_pitch = 8;
+
+Disturbance_F = -100; % [Frontal Plane Disturbance (Forward/Backward)]
+Disturbance_S = 100; % [Sagittal Plane Disturbance (Sideways)]
+
+m_total = 40;
+
+m_body = m_total/2;
+m_legs = m_total/2;
+
+m_ratio = m_body/m_legs;
+TotalForce = sqrt(Disturbance_S^2 + Disturbance_F^2);
+
+fprintf("Ratio: %f ,Disturbance Force: %f \n", m_ratio, TotalForce);
+
+% *************************** %
+% Ground Interaction Model    %
+% *************************** %
+% body mass
+m = 42; %[kg] 
+
+% gravitational acceleration
+g = 9.81; %[m/s^2]
+
+% Vertical component
+% ----------------------
+% stiffness of ground interaction along contact normal axis
+k_gn = m*g/0.01; %[N/m]
+% max relaxation speed of normal ground interaction
+v_gn_max = 0.03; %[m/s]
+
+% Horizontal component
+% ------------------------
+% sliding friction coefficient
+mu_slide = 0.8; %[]
+% stiction to sliding transition coefficient
+mu_stick = 0.9; %[]
+% sliding to stiction transition velocity limit
+vLimit = 0.01; %[m/s]
+% stiffness of ground stiction along contact tangential
+k_gt = m*g/0.01; %[N/m]
+% max relaxation speed of ground stiction
+v_gt_max = 0.03; %[m/s] 
